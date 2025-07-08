fdc233ed4db6ed945aba9451b9d98b121124ec4d
kasam75 <wjdghltjr22@naver.com>
2025-07-08 20:45:13 +0900

    0844

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,44 +12,50 @@ public class Main {
 
         System.out.print("두번째 숫자를 입력해주세요!");
         String str2=scanner.nextLine();
-
+        // 정규 표현식+트라이 케치(가능하면 도전해보기)
         System.out.print("등호 +,-,*,/를 입력해 주세요");
         String str3 = scanner.nextLine ();
 
-
+        //루프문내에 루프문
         //수식
 
         int num1 = Integer.parseInt (str1);
         int num2 = Integer.parseInt (str2);
-        int num3 = Integer.parseInt (str3);
-
 
         //조건문 이용 수식받은걸 나눗샘인지 확인
 
         int result=0;
-        boolean result2= "+".equals(str3);
 
         switch (str3) {
             case "+":
-                System.out.println(num1+num2);
+                result=(num1+num2);
                 break;
             case "-":
-                System.out.println(num1-num2);
+                result=(num1-num2);
                break;
             case "*":
-                System.out.println(num1*num2);
+                result=(num1*num2);
                 break;
             case "/":
-                 System.out.println(num1/num2);
+                result=(num1/num2);
                 break;
             default:
                 System.out.println("등호를 입력해주세요");
-               return;
+                //루프문 컨티뉴로
+                //문자열 비교로
+                return;
         }
 
-        int sum = num1 + num2;
+
+
+
+        //나눗샘은 상수가 아닐떄 처리가 필요함
+        //반복문
+        //예쁘게 꾸미기
+        //특정문자 받았을때 빠져나가게(?)
+
         System.out.println ();
-        System.out.println ("값은" + sum);
+        System.out.println ("값은" + result);
 
 
 
