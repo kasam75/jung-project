65e30dad90b065113c6f1609f107e7b29493dfe3
kasam75 <wjdghltjr22@naver.com>
2025-07-08 14:25:26 +0900

    테스트2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int a;
-        a = 1;
-        a = 2;
+        a = 3;
+        a = 5;
         System.out.println("a = " + a);}
 }
