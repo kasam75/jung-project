fe39d8655cf889da496a6294548283cc46cc2974
kasam75 <wjdghltjr22@naver.com>
2025-07-09 14:19:17 +0900

    LV1_end

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,63 +1,73 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("🧮계산기 인삿말 입니다. \n 연산후'exit'를 누르시면 계산이 종료됩니다. = scanner");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n 🧮계산기 입니다. \n 연산후'exit'를 누르시면 계산이 종료됩니다. = scanner");
 
-        System.out.print("1️⃣첫번째 숫자를 입력해 주세요!");
-        String str1=scanner.nextLine();
+            System.out.print("1️⃣ 첫번째 숫자를 입력해 주세요!");
+            String str1 = scanner.nextLine();
 
-        System.out.print("2️⃣두번째 숫자를 입력해주세요!");
-        String str2=scanner.nextLine();
-        // 정규 표현식+트라이 케치(가능하면 도전해보기)
-        System.out.print("\uD83D\uDD23등호 +,-,*,/를 입력해 주세요");
-        String str3 = scanner.nextLine ();
-       
-        //루프문내에 루프문
-        //수식
+            System.out.print("2️⃣ 두번째 숫자를 입력해주세요!");
+            String str2 = scanner.nextLine();
+            // 정규 표현식+트라이 케치(가능하면 도전해보기)
+            System.out.print("\uD83D\uDD23 등호➕,➖,✖,➗를 입력해 주세요");
+            String str3 = scanner.nextLine();
 
-        int num1 = Integer.parseInt (str1);
-        int num2 = Integer.parseInt (str2);
+            //루프문내에 루프문
+            //수식
 
-        //조건문 이용 수식받은걸 나눗샘인지 확인
+            int num1 = Integer.parseInt(str1);
+            int num2 = Integer.parseInt(str2);
+            //조건문 이용 수식받은걸 나눗샘인지 확인
 
-        int result=0;
+            int result = 0;
 
-        switch (str3) {
-            case "+":
-                result=(num1+num2);
+            switch (str3) {
+                case "+":
+                    result = (num1 + num2);
+                    break;
+                case "-":
+                    result = (num1 - num2);
+                    break;
+                case "*":
+                    result = (num1 * num2);
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.println("⏮0보다 값이 커야합니다.⏮");
+                    }
+                    result = (num1 / num2);
+                    break;
+                default:
+                    System.out.println("❌등호를 입력해주세요❌");
+                    //루프문 컨티뉴로
+                    //문자열 비교로
+                    continue;
+            }
+
+
+            //나눗샘은 상수가 아닐떄 처리가 필요함
+            //반복문  완료
+            //예쁘게 꾸미기
+            //특정문자 받았을때 빠져나가게(?)(절반0입력했을때 오류 )
+            //줄정렬 컨트롤 알트 엘
+            System.out.println();
+            System.out.println("\uD83C\uDD97 " + num1 + " " + str3 + " " + num2 + " = " + result);
+
+            System.out.println("  계산을 계속 하시겠습니까?\n\n👍Please enter any key👍\n           or    \n  💣exit 입력시 종료💣");
+            String next = scanner.nextLine();
+            if (next.equals("exit")) {
+                System.out.println("5💥계산을 종료 합니다.💥");
                 break;
-            case "-":
-                result=(num1-num2);
-               break;
-            case "*":
-                result=(num1*num2);
-                break;
-            case "/":
-                result=(num1/num2);
-                break;
-            default:
-                System.out.println("등호를 입력해주세요");
-                //루프문 컨티뉴로 
-                //문자열 비교로
-                return;
+            }
+
+
         }
-      
-
-
-
-        //나눗샘은 상수가 아닐떄 처리가 필요함
-        //반복문
-        //예쁘게 꾸미기
-        //특정문자 받았을때 빠져나가게(?)
-
-        System.out.println ();
-        System.out.println ("\uD83C\uDD97 값은" + result);
-
-
-
     }
 }
