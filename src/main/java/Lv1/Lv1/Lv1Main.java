35d8bdffe54e549b200e475caedc28f2c3bd71cf
kasam75 <wjdghltjr22@naver.com>
2025-07-14 17:43:14 +0900

    alpa

--- src/main/java/Lv1/Lv1/Lv1Main.java
+++ src/main/java/Lv1/Lv1/Lv1Main.java
@@ -1,8 +1,6 @@
-package Lv1.example;
-
 import java.util.Scanner;
 
-public class Main {
+public class Lv1Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
